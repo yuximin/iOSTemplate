b0301be5118ed2ec0c9c4fb45da6c0d72de7ebd5
yuximin <whaledestine@163.com>
2022-10-17 19:15:11 +0800

    add: 字符串添加正则替换扩展方法

--- iOSTemplate/Extension/String+Extension.swift
+++ iOSTemplate/Extension/String+Extension.swift
@@ -19,3 +19,17 @@ extension String {
         return .systemFont(ofSize: CGFloat(size), weight: weight)
     }
 }
+
+// MARK: - 正则替换
+extension String {
+    public func regexReplacing(pattern: String, withTemplate template: String) -> String {
+        var result = self
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: [])
+            result = regex.stringByReplacingMatches(in: result, range: NSMakeRange(0, result.count), withTemplate: template)
+        } catch {
+            Logger.info("String regexReplacing error: \(error.localizedDescription)")
+        }
+        return result
+    }
+}
