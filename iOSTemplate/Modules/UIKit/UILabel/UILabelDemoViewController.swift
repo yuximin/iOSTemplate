a4eea59563599271e88d3f96b30e418b6ebde2aa
yuximin <whaledestine@163.com>
2023-05-12 12:01:49 +0800

    new - 渐变文本

--- iOSTemplate/Modules/UIKit/UILabel/UILabelDemoViewController.swift
+++ iOSTemplate/Modules/UIKit/UILabel/UILabelDemoViewController.swift
@@ -16,6 +16,14 @@ class UILabelDemoViewController: UIViewController {
         label.backgroundColor = .red.withAlphaComponent(0.8)
         return label
     }()
+    
+    private lazy var colorsLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.backgroundColor = .black
+        label.numberOfLines = 0
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +44,73 @@ class UILabelDemoViewController: UIViewController {
                                     y: (UIScreen.main.bounds.height - boundingRect.height) / 2,
                                     width: boundingRect.width,
                                     height: boundingRect.height)
+        
+        view.addSubview(colorsLabel)
+        colorsLabel.text = "我哦卫计委阿胶粉i哦我饿叫哦😁if金额我ij😁sdfasdfasdfasdfasdfasdfasdfsa"
+        colorsLabel.frame = CGRect(x: 30.0, y: contentLabel.frame.maxY + 10.0, width: 200.0, height: 50.0)
+        
+        if let image = UIImage.imageWith(colors: [UIColor.red, UIColor.blue], size: CGSize(width: 100.0, height: 50.0), gradientType: .horizontal) {
+            let color = UIColor(patternImage: image)
+            colorsLabel.textColor = color
+        } else {
+            colorsLabel.textColor = .red
+        }
     }
 
+}
+
+extension UIImage {
+
+    public enum GradientType3 {
+        /// 水平
+        case horizontal
+        /// 竖直
+        case vertical
+        /// 斜向上
+        case obliqueUpward
+        /// 斜向下
+        case obliqueDownward
+    }
+    /// 生成渐变色图片
+    public static func imageWith(colors: [UIColor], size: CGSize, gradientType: GradientType3 = .vertical, cornerRadius: CGFloat = 0, corners: UIRectCorner = .allCorners, isOpaque: Bool = false) -> UIImage? {
+        var colors = colors
+        if colors.isEmpty {
+            colors = [.white, .white]
+        } else if colors.count == 1 {
+            colors = [colors.first!, colors.first!]
+        }
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.isOpaque = isOpaque
+        gradientLayer.frame = CGRect(origin: .zero, size: size)
+        switch gradientType {
+        case .horizontal:
+            gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+            gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        case .vertical:
+            gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        case .obliqueUpward:
+            gradientLayer.startPoint = CGPoint(x: 0, y: 1)
+            gradientLayer.endPoint = CGPoint(x: 1, y: 0)
+        case .obliqueDownward:
+            gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        }
+
+        if cornerRadius != 0 {
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.frame = CGRect(origin: .zero, size: size)
+            shapeLayer.path = UIBezierPath(roundedRect: CGRect(origin: .zero, size: size), byRoundingCorners: corners, cornerRadii: CGSize(width: cornerRadius, height: cornerRadius)).cgPath
+            gradientLayer.mask = shapeLayer
+        }
+        gradientLayer.colors = colors.map({ $0.cgColor })
+        UIGraphicsBeginImageContextWithOptions(size, gradientLayer.isOpaque, 0)
+        if let context = UIGraphicsGetCurrentContext() {
+            gradientLayer.render(in: context)
+        }
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
 }
